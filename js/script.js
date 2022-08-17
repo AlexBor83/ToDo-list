4a83e7d935836fec6ee9ed239c378478ecf953a6
AlexBor83 <rabota_for_alex@mail.ru>
2022-08-17 22:53:05 +0400

    Сделла задание 13

--- js/script.js
+++ js/script.js
@@ -1,60 +1,66 @@
 'use strict';
 
 const todoControl = document.querySelector('.todo-control');
-const headerInput = document.querySelector('.header-input')
+const headerInput = document.querySelector('.header-input');
 const todoList = document.querySelector('.todo-list');
 const todoCompleted = document.querySelector('.todo-completed');
 
-const toDoData = [];
+const toDoData = JSON.parse(localStorage.getItem('BD'));
+render();
 
-const render = function () {
-    todoList.innerHTML = '';
-    todoCompleted.innerHTML = '';
+function render () {
+  todoList.innerHTML = '';
+  todoCompleted.innerHTML = '';
 
-    toDoData.forEach(function(item) {
-        const li = document.createElement('li');
-        li.classList.add('todo-item');
-        li.innerHTML = '<span class="text-todo">' + item.text + '</span>' + 
-            '<div class="todo-buttons">' +
-            '<button class="todo-remove"></button>'+
-            '<button class="todo-complete"></button>'+
-            '</div>';
-        
-        if (item.completed) {
-            todoCompleted.append(li);            
-        } else {
-            todoList.append(li); 
-        }
+  toDoData.forEach(function (item, i, arr) {
+    const li = document.createElement('li');
+    li.classList.add('todo-item');
+    li.innerHTML =
+      '<span class="text-todo">' +
+      item.text +
+      '</span>' +
+      '<div class="todo-buttons">' +
+      '<button class="todo-remove"></button>' +
+      '<button class="todo-complete"></button>' +
+      '</div>';
 
-        li.querySelector('.todo-complete')
-          .addEventListener('click', function() {            
-            item.completed = !item.completed;
-            render();            
-        });
+    if (item.completed) {
+      todoCompleted.append(li);
+    } else {
+      todoList.append(li);
+    }
 
-        
-
-               
-        
-
+    li.querySelector('.todo-complete').addEventListener('click', function () {
+      item.completed = !item.completed;
+      render();
     });
-    
+
+    li.querySelector('.todo-remove').addEventListener('click', function () {
+      li.remove();
+      arr.splice(i, 1);
+      localStorage.clear();
+      localStorage.setItem('BD', JSON.stringify(toDoData));      
+    });
+  });
+
+  localStorage.clear();
+  localStorage.setItem('BD', JSON.stringify(toDoData));
 };
 
 todoControl.addEventListener('submit', function (e) {
-    e.preventDefault();
-    
+  e.preventDefault();
+
+  if (headerInput.value.trim() !== '') {
     const newToDo = {
         text: headerInput.value,
-        completed: false
-    };
-
-    toDoData.push(newToDo);
-    render();
-
-    headerInput.value = '';    
+        completed: false,
+      };
+    
+      toDoData.push(newToDo);
+      render();
+    
+      headerInput.value = '';
+  } else {
+    alert('Заполните поле');
+  }  
 });
-
-
-
-
